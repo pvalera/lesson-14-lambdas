e90283d09f85e46377a02b2483b47427bed4b4c0
unknown <pvalera@hotmail.com>
2020-05-25 23:39:39 -0700

    Print planets in different sort criteria

--- src/exercises/_4_SolarSystem.java
+++ src/exercises/_4_SolarSystem.java
@@ -1,9 +1,11 @@
 package exercises;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class _3_SolarSystem {
+public class _4_SolarSystem {
 
 	public static void main(String[] args) {
 		List<Planet> planets = Arrays.asList(
@@ -14,16 +16,28 @@ public class _3_SolarSystem {
 				new Planet("Earth", 150, 1, 1),
 				new Planet("Saturn", 1430, 763.59, 62)
 				);
-		
 
 		// 1: Sort the planets by distance from the sun
+		System.out.println("Planets sorted by distance from the sun:");
+		Collections.sort(planets, Collections.reverseOrder((p1, p2) -> p2.getDistanceFromSun() - p1.getDistanceFromSun()));		
+		for(int i=0;i<planets.size();i++){
+		    System.out.println(planets.get(i).getName() + " " + planets.get(i).getDistanceFromSun());
+		} 		
 		
 		// 2. Sort the planets in alphabetical order
+		planets.sort(Comparator.comparing(Planet::getName));
+		System.out.println("Planets sorted by alphabetical order:");
+		for(int i=0;i<planets.size();i++){
+		    System.out.println(planets.get(i).getName());
+		} 		
 		
 		// 3. Sort planets from largest to smallest
+		planets.sort(Collections.reverseOrder(Comparator.comparing(Planet::getVolume)));
+		System.out.println("Planets sorted by volume:");		
+		for(int i=0;i<planets.size();i++){
+		    System.out.println(planets.get(i).getName() + " " + planets.get(i).getVolume());
+		} 		
 		
-
 	}
-
 }
 
