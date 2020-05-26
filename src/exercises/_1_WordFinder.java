5a5aa615c566c9971e5a7a27c0518cce1c49a5f7
unknown <pvalera@hotmail.com>
2020-05-25 23:32:45 -0700

    Print words that start with P and over 10 characters

--- src/exercises/_1_WordFinder.java
+++ src/exercises/_1_WordFinder.java
@@ -6,14 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class _1_WordFinder {
 
 	public static void main(String[] args) {
-		/* Print all the words beginning with P that are over 10 characters. */
+		/* Print all the words beginning with P that are over 10 characters. */		
+		List<String> words = loadWordList();
+		Stream<String> streamOfWords = words.stream();
 		
-
+		List<String> resultWords = streamOfWords
+				.filter(word -> word.toUpperCase().charAt(0)=='P')
+				.filter(word -> word.length() > 10)
+				.collect(Collectors.toList());
 		
+		System.out.println(resultWords);	
 	}
 
 	public static List<String> loadWordList() {
@@ -26,10 +34,10 @@ public class _1_WordFinder {
 			while ((aLine = buffer.readLine()) != null) {
 				messages.add(aLine);
 			}
+			buffer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return messages;
 	}
 
